642c1cd7916328511c5d160834820286624c1c37
Steve Ziuchkovski <zman97211@gmail.com>
2014-04-12 20:35:37 -0400

    Logs with Boost trivial logging facilities.

--- LogService/logger.cpp
+++ LogService/logger.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
+#include <boost/log/trivial.hpp>
+
 #include "logger.h"
 
 rr::logger::logger(const std::string& module) : _module{ module } {
 }
 
 void rr::logger::log(const std::string& msg) const {
-	std::cout << _module << ": " << msg << std::endl;
+	BOOST_LOG_TRIVIAL(info) << msg;
 }
